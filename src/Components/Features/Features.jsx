48de745a615cb1c0f9f7887aba286f6ec6ef6d5d
PRAKASH RAJA <119003888+prakashraja-0723@users.noreply.github.com>
2023-08-28 20:46:09 +0530

    Update Features.jsx

--- src/Components/Features/Features.jsx
+++ src/Components/Features/Features.jsx
@@ -8,17 +8,17 @@ const Features = () => {
       content: `Our modern web and mobile applications allow you to keep track of your finances wherever you are in the world.`,
     },
 		{
-			img: "/src/assets/images/icon-budgeting.svg",
+			img: "../../src/assets/images/icon-budgeting.svg",
 			title: "Simple Budgeting",
 			content: `See exactly where your money goes each month. Receive notifications when you're close to your hitting limits.`,
 		},
 		{
-			img: "/src/assets/images/icon-onboarding.svg",
+			img: "../../src/assets/images/icon-onboarding.svg",
 			title: "Fast Onboarding",
 			content: `We don't do branches. Open your account in minutes online and start taking control of your finances right away.`,
 		},
 		{
-			img: "/src/assets/images/icon-api.svg",
+			img: "../../src/assets/images/icon-api.svg",
 			title: "Open API",
 			content: `Manage your savings, investments, pension, and much more from one account. Tracking your money has never been easier.`,
 		},
