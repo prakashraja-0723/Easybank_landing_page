7f247d82965e63c0b5defeeaf2372db0b6c3a168
PRAKASH RAJA <prakashraja0723@outlook.com>
2023-08-28 22:26:02 +0530

    updated

--- src/Components/UI_Components/FeatureList.jsx
+++ src/Components/UI_Components/FeatureList.jsx
@@ -3,7 +3,7 @@ const FeatureList = (props) => {
 	const feature_class={
 		ul_class:`flex items-center justify-center flex-col gap-14 py-14 w-10/12 xl:w-full xl:gap-[30px] xl:flex-row  xl:w-full xl:items-start xl:justify-start xl:py-20`,
 		li_class:`flex items-center justify-center flex-col gap-5 xl:items-start xl:justify-start`,
-		h3_class:`text-2xl text-c_dark_blue`,
+		h1_class:`text-2xl text-c_dark_blue`,
 		p_class:`text-sm text-c_grayish_blue xl:w-[95%] xl:text-base`
 	}
 	return (
@@ -15,9 +15,9 @@ const FeatureList = (props) => {
             alt="Online Banking"
             className={``}
           />
-          <h3 className={feature_class.h3_class}>
+          <h1 className={feature_class.h1_class}>
             {props.Feature_Online.title}
-          </h3>
+          </h1>
           <p className={feature_class.p_class}>
             {props.Feature_Online.content}
           </p>
@@ -28,9 +28,9 @@ const FeatureList = (props) => {
             alt="Simple Budgeting"
             className={``}
           />
-          <h3 className={feature_class.h3_class}>
+          <h1 className={feature_class.h1_class}>
             {props.Feature_Budgeting.title}
-          </h3>
+          </h1>
           <p className={feature_class.p_class}>
             {props.Feature_Budgeting.content}
           </p>
@@ -41,16 +41,16 @@ const FeatureList = (props) => {
             alt="Fast Onboarding"
             className={``}
           />
-          <h3 className={feature_class.h3_class}>
+          <h1 className={feature_class.h1_class}>
             {props.Feature_Onboarding.title}
-          </h3>
+          </h1>
           <p className={feature_class.p_class}>
             {props.Feature_Onboarding.content}
           </p>
         </li>
         <li className={feature_class.li_class}>
           <img src={props.Feature_Api.img} alt="Open Api" className={``} />
-          <h3 className={feature_class.h3_class}>{props.Feature_Api.title}</h3>
+          <h1 className={feature_class.h1_class}>{props.Feature_Api.title}</h1>
           <p className={feature_class.p_class}>{props.Feature_Api.content}</p>
         </li>
       </ul>
