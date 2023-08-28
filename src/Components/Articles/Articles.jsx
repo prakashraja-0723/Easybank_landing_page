65bfccb123d971bb796dd4bcc295fff0cfaf4d6d
PRAKASH RAJA <119003888+prakashraja-0723@users.noreply.github.com>
2023-08-28 20:37:32 +0530

    Update Articles.jsx

--- src/Components/Articles/Articles.jsx
+++ src/Components/Articles/Articles.jsx
@@ -3,25 +3,25 @@ import ArticleCard from "../UI_Components/ArticleCard.jsx";
 const Articles = () => {
   const article_card_info = [
     {
-      img: "/src/assets/images/image-currency.jpg",
+      img: "../Articles/Components/src/assets/images/image-currency.jpg",
       user_name:'Claire Robinson',
       article_title: `Receive money in any currency with no fees`,
       article_content:`The world is getting smaller and were becoming more mobile. So why should you be forced to only receive money in a single ...`
     },
     {
-      img: "/src/assets/images/image-restaurant.jpg",
+      img: "../Articles/Components/src/assets/images/image-restaurant.jpg",
       user_name:'Wilson Hutton',
       article_title: `Treat yourself without worrying about money`,
       article_content:`Our simple budgeting feature allows you to separate out your spending and set realistic limits each month. That means you ...`
     },
     {
-      img: "/src/assets/images/image-plane.jpg",
+      img: "../Articles/Components/src/assets/images/image-plane.jpg",
       user_name:'Claire Robinson',
       article_title: `Take your Easybank card wherever you go`,
       article_content:`We want you to enjoy your travels. This IS why we don't charge any fees on purchases while you're abroad. We'll even show you ...`
     },
     {
-      img: "/src/assets/images/image-confetti.jpg",
+      img: "../Articles/Components/src/assets/images/image-confetti.jpg",
       user_name:'Wilson Hutton',
       article_title: `Our invite-only Beta accounts are now live!`,
       article_content:`After a lot of hard work by the whole team. we're excited to launch our closed beta. It's easy to request an invite through the site ...`
