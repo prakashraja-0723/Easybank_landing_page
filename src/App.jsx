3f9dc4906414fd835f4e838a5ac64c5dc26e78fb
PRAKASH RAJA <119003888+prakashraja-0723@users.noreply.github.com>
2023-08-28 20:44:19 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -35,35 +35,35 @@ const App = () => {
             className={`py-12 px-12 flex flex-col items-center justify-start gap-3 xl:flex-row xl:h-fit  lg:min-w-[65rem] lg:m-auto lg:max-w-[75rem] lg:py-3 lg:px-10 `}
           >
             <div className={` flex items-center justify-center flex-col gap-6 xl:mr-44 lg:items-start`}>
-              <img src="/src/assets/images/logo-white.svg" alt="logo" />
+              <img src="./src/assets/images/logo-white.svg" alt="logo" />
               <ul className={`flex items-center justify-center gap-5`}>
                 <li>
                   <img
-                    src="/src/assets/images/icon-facebook.svg"
+                    src="./src/assets/images/icon-facebook.svg"
                     alt="_facebooklogo"
                   />
                 </li>
                 <li>
                   <img
-                    src="/src/assets/images/icon-youtube.svg"
+                    src="./src/assets/images/icon-youtube.svg"
                     alt="_youtubelogo"
                   />
                 </li>
                 <li>
                   <img
-                    src="/src/assets/images/icon-twitter.svg"
+                    src="./src/assets/images/icon-twitter.svg"
                     alt="_twitterlogo"
                   />
                 </li>
                 <li>
                   <img
-                    src="/src/assets/images/icon-pinterest.svg"
+                    src="./src/assets/images/icon-pinterest.svg"
                     alt="_pinterestlogo"
                   />
                 </li>
                 <li>
                   <img
-                    src="/src/assets/images/icon-instagram.svg"
+                    src="./src/assets/images/icon-instagram.svg"
                     alt="_instagramlogo"
                   />
                 </li>
